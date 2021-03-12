6cc9f4b622d19b8485a6a6e8a3b762f296aec59d
Brynne98 <36798170+Brynne98@users.noreply.github.com>
2021-03-12 15:02:54 +0200

    Added new endpoints

--- src/index.js
+++ src/index.js
@@ -19,6 +19,28 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+
+    User.find({}).then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        res.status(500).send();
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+
+    User.findById(req.params.id).then((result) => {
+        if(!result) {
+            res.status(404).send();
+        }
+
+        res.send(result)
+    }).catch((error) => {
+        res.status(500).send();
+    })
+})
+
 app.post('/tasks', (req, res) => {
 
     const task = new Task(req.body);
@@ -27,6 +49,28 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(result);
     }).catch((error) => {
         res.status(400).send(error);
+    })
+})
+
+app.get('/tasks', (req, res) => {
+
+    Task.find().then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+
+    Task.findById(req.params.id).then((result) => {
+        if(!result) {
+            res.status(404).send();
+        }
+
+        res.send(result);
+    }).catch((error) => {
+        res.status(500).send()
     })
 })
 
